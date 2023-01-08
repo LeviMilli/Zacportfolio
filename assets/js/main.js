9606296834c959124e85a32a38f0f916268c7b7e
unknown <levi.millikin@gmail.com>
2023-01-08 15:36:31 -0800

    loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,14 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+import CMS from 'netlify-cms-app'
+// Initialize the CMS object
+CMS.init()
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate('my-template', MyTemplate)
+
+
+
 (function($) {
 
 	var	$window = $(window),
